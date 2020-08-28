3739ba200865f394cb850c06c91dc24ccdb0ba26
kuduzow <admin@intocode.ru>
2020-08-28 20:32:51 +0300

    add lastMessage lookup

--- models/Contact.js
+++ models/Contact.js
@@ -6,8 +6,6 @@ const contactSchema = new Schema({
   username: String,
   fullname: String,
   picture: String,
-  lastMessage: String,
-  lastMessageDate: Date,
   lastVisit: Date,
   archived: {
     type: Boolean,
--- routes/contacts.route.js
+++ routes/contacts.route.js
@@ -1,10 +1,33 @@
 const { Router } = require('express');
 const Contact = require('../models/Contact');
+const Message = require('../models/Message');
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const contacts = await Contact.find({});
+  const contacts = await Contact.aggregate([
+    {
+      $lookup: {
+        from: Message.collection.name,
+        let: { id: '$_id' },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $or: [
+                  { $eq: ['$fromUserId', '$$id'] },
+                  { $eq: ['$toUserId', '$$id'] }
+                ]
+              }
+            }
+          },
+          { $sort: { time: -1 } },
+          { $limit: 1 }
+        ],
+        as: 'lastMessage'
+      }
+    },
+  ]);
 
   return res.json(contacts);
 });
